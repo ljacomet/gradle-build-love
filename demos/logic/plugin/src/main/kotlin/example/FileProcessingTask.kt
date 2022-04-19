a55ead4137a16cadbc456c86b3619cf2ebc70638
paul <paul@gradle.com>
2022-04-20 00:26:35 +0200

    demo: logic step 1

---
+++ demos/logic/plugin/src/main/kotlin/example/FileProcessingTask.kt
@@ -0,0 +1,48 @@
+package example
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.file.DirectoryProperty
+import org.gradle.api.provider.Property
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.InputFiles
+import org.gradle.api.tasks.OutputDirectory
+import org.gradle.api.tasks.SkipWhenEmpty
+import org.gradle.api.tasks.TaskAction
+import java.io.File
+
+
+abstract class FileProcessingTask : DefaultTask() {
+
+    @get:Input
+    abstract val processing: Property<String>
+
+    @get:InputFiles
+    @get:SkipWhenEmpty
+    abstract val inputDirectory: DirectoryProperty
+
+    @get:OutputDirectory
+    abstract val outputDirectory: DirectoryProperty
+
+    @TaskAction
+    fun action() {
+        val outputDir = outputDirectory.get().asFile
+        outputDir.deleteRecursively()
+        outputDir.mkdirs()
+        val inputDir = inputDirectory.get().asFile
+        inputDirectory.get().asFileTree.files.forEach { inputFile ->
+            processFile(
+                processing = processing.get(),
+                inputFile = inputFile,
+                outputFile = outputDir.resolve(inputFile.relativeTo(inputDir).path)
+            )
+        }
+    }
+
+    private
+    fun processFile(processing: String, inputFile: File, outputFile: File) {
+        outputFile.run {
+            parentFile.mkdirs()
+            writeText(processing)
+        }
+    }
+}
