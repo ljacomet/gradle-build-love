425502b110a72fd3f9e214e4d2babd043b378f0d
Kyle Moore <kmoore@gradle.com>
2022-10-09 21:58:43 +0200

    Create configuration with default dependencies and wire to task input

--- demos/logic/plugin4/src/main/kotlin/file-processing.gradle.kts
+++ demos/logic/plugin4/src/main/kotlin/file-processing.gradle.kts
@@ -7,8 +7,15 @@ plugins {
 
 val fileProcessing = extensions.create<FileProcessingExtension>("fileProcessing")
 
+val guavaCoordinates = "com.google.guava:guava:31.1-jre"
+
+val fooConfiguration = configurations.create("fileProcessingTool").defaultDependencies {
+    this.add(project.dependencies.create(guavaCoordinates))
+}
+
 tasks.register<FileProcessingTask>("processFiles") {
     processing.set(fileProcessing.processing)
+    toolClasspath.from(fooConfiguration)
     inputDirectory.set(layout.projectDirectory.dir("input"))
     outputDirectory.set(layout.buildDirectory.dir("output"))
 }
